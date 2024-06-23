3ba938d9449a4b7dde45a6a438814cd69f83a638
Shay Cohen <shayco@gmail.com>
2024-06-23 09:09:53 +0300

    updated python client

--- client.py
+++ client.py
@@ -1,20 +1,25 @@
 from keycloak import KeycloakOpenID
 
-# Configure client
+# Initiate client
 keycloak_openid = KeycloakOpenID(server_url="http://keycloak:8080/auth/",
                                  client_id="app-html5",
                                  realm_name="quickstart",
                                  client_secret_key="secret")
 
-# Get WellKnown
-# config_well_known = keycloak_openid.well_known()
+# Get WellKnown Configuration
+try: 
+    config_well_know = keycloak_openid.well_know()
+    print('certs', keycloak_openid.certs())
+except Exception as e: 
+    print(e, flush=True)
+    import pdb; pdb.set_trace()
 
-# Get Token
-token = keycloak_openid.token("alice", "password")
-
-# Get Userinfo
-userinfo = keycloak_openid.userinfo(token['access_token'])
-print('token', token)
-print('userinfo', userinfo)
-print('certs', keycloak_openid.certs())
-
+# Get a User Token
+try: 
+    token = keycloak_openid.token("alice", "password")
+    userinfo = keycloak_openid.userinfo(token['access_token'])
+    print('token', token)
+    print('userinfo', userinfo)
+except Exception as e: 
+    print(e, flush=True)
+    import pdb; pdb.set_trace()
